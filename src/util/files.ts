473a3a87bcc340a10e0aa8aabc22c22ddb39b51f
Jake Hoffman <jaketh19@gmail.com>
2021-02-26 01:13:03 -0500

    for andrei <3

--- src/util/files.ts
+++ src/util/files.ts
@@ -1,13 +1,13 @@
 import fs from 'fs';
 
-const readdir = (dir: fs.PathLike, fn: (arg0: string[]) => void) => {
-    fs.readdir(dir, function(err, f) {
-        if (err) {
-            return console.log('Unable to scan directory: ' + err);
-        } else {
-            fn(f);
-        }
-    })
-}
+// const readdir = (dir: fs.PathLike, fn: (arg0: string[]) => void) => {
+//     fs.readdir(dir, function(err, f) {
+//         if (err) {
+//             return console.log('Unable to scan directory: ' + err);
+//         } else {
+//             fn(f);
+//         }
+//     })
+// }
 
 export default { readdir };
